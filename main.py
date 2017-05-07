d66fa17fcff25070a573d0799b541e6b4a5b5d6b
StevenRitz <sgr0003@gmail.com>
2017-05-07 05:51:43 -0700

    It might actually work now

--- main.py
+++ main.py
@@ -84,13 +84,16 @@ class RedirectPage(webapp2.RequestHandler):
 		token_response = urllib.urlopen("https://accounts.google.com/o/oauth2/token", urllib.urlencode(token_params))
 		#get the token
 		token_json = json.load(token_response)
-		self.response.write(json.dumps(token_json))
-		#token_string = token_json['access_token']
+		#self.response.write(json.dumps(token_json))
+		token_string = token_json['access_token']
 
-		#token_get_params = {"Authorization" : "bearer" + token_string}
+		token_get_params = {"Authorization" : "bearer" + token_string}
 
+		google_response = urllib.urlopen(API_URL % token_get_params)
 		#google_response = urllib.urlopen(API_URL % token_get_params)
-		#google_response = urllib.urlopen(API_URL % token_get_params)
+		google_response_json = json.load(google_response)
+		self.response.write(google_response_json['name']['givenName'] + '\n')
+		self.response.write(google_response_json['name']['familyName'] + '\n')
 
 class DisplayPage(webapp2.RequestHandler):
 	def get(self):
